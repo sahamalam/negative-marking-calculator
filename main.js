9843a51abd5f3f9f1270d6e40d881a1d3bf2ee39
sahamalam <sahamalam@gmail.com>
2024-03-24 12:45:08 +0530

    major

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
 function _0x25f9() {
-    var _0x21160e = ['497563ffEXgH', '226805wtobix', '30fsuTnF', '10fhYVwL', '179004VXoylm', '29439xVzkQL', '845154iuIWdT', 'getElementById', '4174569YCGvNd', 'value', '1688DOKabN', '345635blBpkd', '8qKENAP'];
+    var _0x21160e = [
+        "497563ffEXgH",
+        "226805wtobix",
+        "30fsuTnF",
+        "10fhYVwL",
+        "179004VXoylm",
+        "29439xVzkQL",
+        "845154iuIWdT",
+        "getElementById",
+        "4174569YCGvNd",
+        "value",
+        "1688DOKabN",
+        "345635blBpkd",
+        "8qKENAP",
+    ];
     _0x25f9 = function () {
         return _0x21160e;
     };
@@ -8,30 +22,59 @@ function _0x25f9() {
 
 function _0x2df0(_0x3506de, _0x4dd340) {
     var _0x25f9c9 = _0x25f9();
-    return _0x2df0 = function (_0x2df05b, _0x15b412) {
-        _0x2df05b = _0x2df05b - 0x1c2;
-        var _0x59bf9b = _0x25f9c9[_0x2df05b];
-        return _0x59bf9b;
-    }, _0x2df0(_0x3506de, _0x4dd340);
-}(function (_0x7a201e, _0x498075) {
+    return (
+        (_0x2df0 = function (_0x2df05b, _0x15b412) {
+            _0x2df05b = _0x2df05b - 0x1c2;
+            var _0x59bf9b = _0x25f9c9[_0x2df05b];
+            return _0x59bf9b;
+        }),
+        _0x2df0(_0x3506de, _0x4dd340)
+    );
+}
+(function (_0x7a201e, _0x498075) {
     var _0x1805be = _0x2df0,
         _0x3c9f36 = _0x7a201e();
     while (!![]) {
         try {
-            var _0x35ee1e = parseInt(_0x1805be(0x1ca)) / 0x1 + -parseInt(_0x1805be(0x1c2)) / 0x2 + parseInt(_0x1805be(0x1cd)) / 0x3 * (parseInt(_0x1805be(0x1c8)) / 0x4) + parseInt(_0x1805be(0x1c7)) / 0x5 * (parseInt(_0x1805be(0x1cb)) / 0x6) + -parseInt(_0x1805be(0x1c4)) / 0x7 + parseInt(_0x1805be(0x1c6)) / 0x8 * (parseInt(_0x1805be(0x1ce)) / 0x9) + -parseInt(_0x1805be(0x1cc)) / 0xa * (-parseInt(_0x1805be(0x1c9)) / 0xb);
+            var _0x35ee1e =
+                parseInt(_0x1805be(0x1ca)) / 0x1 +
+                -parseInt(_0x1805be(0x1c2)) / 0x2 +
+                (parseInt(_0x1805be(0x1cd)) / 0x3) *
+                (parseInt(_0x1805be(0x1c8)) / 0x4) +
+                (parseInt(_0x1805be(0x1c7)) / 0x5) *
+                (parseInt(_0x1805be(0x1cb)) / 0x6) +
+                -parseInt(_0x1805be(0x1c4)) / 0x7 +
+                (parseInt(_0x1805be(0x1c6)) / 0x8) *
+                (parseInt(_0x1805be(0x1ce)) / 0x9) +
+                (-parseInt(_0x1805be(0x1cc)) / 0xa) *
+                (-parseInt(_0x1805be(0x1c9)) / 0xb);
             if (_0x35ee1e === _0x498075) break;
-            else _0x3c9f36['push'](_0x3c9f36['shift']());
+            else _0x3c9f36["push"](_0x3c9f36["shift"]());
         } catch (_0x4d1db4) {
-            _0x3c9f36['push'](_0x3c9f36['shift']());
+            _0x3c9f36["push"](_0x3c9f36["shift"]());
         }
     }
-}(_0x25f9, 0x63ab6), calculate = function () {
+})(_0x25f9, 0x63ab6),
+(calculate = function () {
     var _0x3cdcf6 = _0x2df0,
-        _0x5279d4 = document[_0x3cdcf6(0x1c3)]('a1')[_0x3cdcf6(0x1c5)],
-        _0x1b48cb = document[_0x3cdcf6(0x1c3)]('b1')[_0x3cdcf6(0x1c5)],
+        _0x5279d4 = document[_0x3cdcf6(0x1c3)]("a1")[_0x3cdcf6(0x1c5)],
+        _0x1b48cb = document[_0x3cdcf6(0x1c3)]("b1")[_0x3cdcf6(0x1c5)],
         _0x1a248f = _0x1b48cb / _0x5279d4,
-        _0x2abbee = document[_0x3cdcf6(0x1c3)]('a2')[_0x3cdcf6(0x1c5)],
-        _0x468332 = document[_0x3cdcf6(0x1c3)]('a4')[_0x3cdcf6(0x1c5)],
-        _0x19214d = document[_0x3cdcf6(0x1c3)]('a5')[_0x3cdcf6(0x1c5)];
-    document[_0x3cdcf6(0x1c3)]('a6')['innerHTML'] = _0x2abbee * _0x1a248f - _0x468332 * _0x1a248f - _0x468332 * _0x1a248f * '1' / _0x19214d;
+        _0x2abbee = document[_0x3cdcf6(0x1c3)]("a2")[_0x3cdcf6(0x1c5)],
+        _0x468332 = document[_0x3cdcf6(0x1c3)]("a4")[_0x3cdcf6(0x1c5)],
+        _0x19214d = document[_0x3cdcf6(0x1c3)]("a5")[_0x3cdcf6(0x1c5)];
+    document[_0x3cdcf6(0x1c3)]("a6")["innerHTML"] =
+        _0x2abbee * _0x1a248f -
+        _0x468332 * _0x1a248f -
+        (_0x468332 * _0x1a248f * "1") / _0x19214d;
 });
+
+
+function clearFields() {
+    (document.getElementById("a1").value = ""),
+      (document.getElementById("b1").value = ""),
+      (document.getElementById("a2").value = ""),
+      (document.getElementById("a4").value = ""),
+      (document.getElementById("a5").value = ""),
+      (document.getElementById("a6").innerHTML = "");
+  }
